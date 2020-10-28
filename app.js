bd1c2f78b10a0dc482851fd0361e91dc7d26b92b
Nelle <nellebreyes@gmail.com>
2020-10-28 04:40:59 -0400

    redeploy

--- app.js
+++ app.js
@@ -41,14 +41,4 @@ app.use(cors());
 //user routes
 app.use("/api", require("./router"));
 
-//Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/built"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 module.exports = app;
